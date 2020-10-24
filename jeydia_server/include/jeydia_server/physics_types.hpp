8debbc10123f6fdb53023e160e83f0df75d7c01f
Aymeric Pellé <aympelle@gmail.com>
2020-10-24 23:10:48 +0200

    v0.1: Define orientation types.

--- jeydia_server/include/jeydia_server/physics_types.hpp
+++ jeydia_server/include/jeydia_server/physics_types.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <grid/grid_types.hpp>
+#include <dirn/cartographic_directions4.hpp>
 
 namespace jeydia
 {
 
 using Position = grid::grid_position;
 constexpr Position bad_position = Position(-1,-1);
+using Direction = dirn::direction4;
+using Cartographic_directions = dirn::cartographic_directions4;
 
 }
