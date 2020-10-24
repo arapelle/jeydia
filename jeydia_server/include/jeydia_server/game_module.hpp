4005a0125493dd91803842e2b7bccc18d4d90400
Aymeric Pellé <aympelle@gmail.com>
2020-10-24 23:25:33 +0200

    v0.1: Game_module read map from file and listen to physics events.

--- jeydia_server/include/jeydia_server/game_module.hpp
+++ jeydia_server/include/jeydia_server/game_module.hpp
@@ -2,12 +2,14 @@
 
 #include "map.hpp"
 #include "module.hpp"
+#include <evnt/event_listener.hpp>
 #include <wgen/default_syllabary.hpp>
 
 namespace jeydia
 {
 
-class Game_module : public Loop_module<Game_module>
+class Game_module : public Loop_module<Game_module>,
+                    public evnt::event_listener<Moved_in_event, Moved_out_event>
 {
 private:
     using Base_ = Loop_module<Game_module>;
@@ -22,6 +24,16 @@ public:
     inline const Map& map() const { return map_; }
     inline Map& map() { return map_; }
     Agent_id generate_agent_id() const;
+
+public:
+    void receive(Moved_in_event& event);
+    void receive(Moved_out_event& event);
+
+private:
+    void agent_absorbs_energy_(Moved_in_event& event, Agent& agent, Energy_entity& energy);
+
+private:
+    void read_map_from_file_();
 
 private:
     Map map_;
--- jeydia_server/src/game_module.cpp
+++ jeydia_server/src/game_module.cpp
@@ -6,15 +6,11 @@ namespace jeydia
 
 void Game_module::init()
 {
-    std::filesystem::path map_file;
+    read_map_from_file_();
+    map_.set_program_tools(logger(), event_manager());
 
-    appt::program_args args = app().args();
-    if (args.argc >= 4)
-        map_file = app().virtual_filesystem().real_path(args[3]);
-    if (std::filesystem::exists(map_file))
-        if (!map_.read_from_file(map_file))
-            map_.clear();
-    SPDLOG_LOGGER_INFO(logger(), map_);
+    event_manager().connect<Moved_in_event>(*this);
+    event_manager().connect<Moved_out_event>(*this);
 }
 
 void Game_module::run_loop(appt::seconds dt)
@@ -24,6 +20,46 @@ void Game_module::run_loop(appt::seconds dt)
 Agent_id Game_module::generate_agent_id() const
 {
     return syllabary_.random_name64(sizeof(Agent_id));
+}
+
+void Game_module::receive(Moved_in_event& event)
+{
+    Agent* agent = dynamic_cast<Agent*>(&event.first_body());
+    Energy_entity* energy = dynamic_cast<Energy_entity*>(&event.second_body());
+    if (agent && energy)
+        agent_absorbs_energy_(event, *agent, *energy);
+    else
+    {
+        agent = dynamic_cast<Agent*>(&event.second_body());
+        energy = dynamic_cast<Energy_entity*>(&event.first_body());
+        if (agent && energy)
+            agent_absorbs_energy_(event, *agent, *energy);
+    }
+}
+
+void Game_module::receive(Moved_out_event& event)
+{
+}
+
+void Game_module::agent_absorbs_energy_(Moved_in_event& event, Agent& agent, Energy_entity& energy)
+{
+    agent.energy() += energy.count();
+    map_.remove_entity(energy);
+}
+
+void Game_module::read_map_from_file_()
+{
+    const appt::program_args& args = app().args();
+
+    std::filesystem::path map_file;
+    if (args.argc >= 4)
+        map_file = app().virtual_filesystem().real_path(args[3]);
+
+    if (std::filesystem::exists(map_file))
+        if (!map_.read_from_file(map_file))
+            map_.clear();
+
+    SPDLOG_LOGGER_INFO(logger(), map_);
 }
 
 }
