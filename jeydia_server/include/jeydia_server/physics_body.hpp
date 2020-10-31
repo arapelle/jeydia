010bbc781bfe6d93673efd7cdb032aa8ee9fc1b3
Aymeric Pellé <aympelle@gmail.com>
2020-10-31 15:18:37 +0100

    v0.1:JDIA#8: Physics_body has all properties needed.
    dynamic or static
    solid or traversable
    levitating or following gravity
    force and resistance

--- jeydia_server/include/jeydia_server/physics_body.hpp
+++ jeydia_server/include/jeydia_server/physics_body.hpp
@@ -8,13 +8,22 @@ namespace jeydia
 class Physics_body
 {
 public:
-    inline bool is_solid() const { return solid_; }
-    inline bool is_pushable() const { return pushable_; }
+    Physics_body(void* user_data = nullptr);
+    Physics_body(bool dynamic, bool solid, void* user_data = nullptr);
     inline const Position& position() const { return position_; }
-    inline bool is_placed() const { return position_ != bad_position; }
-
+    inline bool is_static() const { return !dynamic_; }
+    inline bool is_dynamic() const { return dynamic_; }
+    inline bool is_solid() const { return solid_; }
+    inline bool is_traversable() const { return !solid_; }
+    inline bool is_levitating() const { return levitating_; }
+    inline bool is_placed() const { return placed_in_world_counter > 0; }
     void set_solid(bool solid);
-    inline void set_pushable(bool pushable) { pushable_ = pushable; }
+    inline void set_levitating(bool levitating) { levitating_ = levitating; }
+
+    inline uint8_t force() const { return force_; }
+    inline uint8_t& force() { return force_; }
+    inline uint8_t resistance() const { return resistance_; }
+    inline uint8_t& resistance() { return resistance_; }
 
     template <class User_data_type = void>
     const User_data_type* user_data() const { return reinterpret_cast<const User_data_type*>(user_data_); }
@@ -25,12 +34,18 @@ public:
 private:
     friend class Map;
     inline Position& mutable_position() { return position_; }
+    inline void inc_placed_in_world_counter() { ++placed_in_world_counter; }
+    inline void dec_placed_in_world_counter() { --placed_in_world_counter; }
 
 private:
     Position position_ = bad_position;
+    bool dynamic_ : 1 = true;
     bool solid_ : 1 = true;
-    bool pushable_ : 1 = true;
+    bool levitating_ : 1 = false;
+    uint8_t force_ = 0;
+    uint8_t resistance_ = 1;
     void* user_data_ = nullptr;
+    uint32_t placed_in_world_counter = 0;
 };
 
 }
--- jeydia_server/src/physics_body.cpp
+++ jeydia_server/src/physics_body.cpp
@@ -3,6 +3,12 @@
 namespace jeydia
 {
 
+Physics_body::Physics_body(void *user_data)
+    : user_data_(user_data)
+{}
 
+Physics_body::Physics_body(bool dynamic, bool solid, void* user_data)
+    : dynamic_(dynamic), solid_(solid), user_data_(user_data)
+{}
 
 }
